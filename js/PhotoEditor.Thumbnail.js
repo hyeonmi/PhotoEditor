814b807c3556652bc7cdc3fb1d21232c49596052
hyeonmi <pilotfish22@gmail.com>
2013-09-23 18:21:39 +0900

    another multi file upload

--- js/PhotoEditor.Thumbnail.js
+++ js/PhotoEditor.Thumbnail.js
@@ -16,7 +16,6 @@ PhotoEditor.Thumbnail = function (options) {
 PhotoEditor.Thumbnail.prototype = {
     init: function () {
         this._setElement();
-        this._attachEvent();
         this.index = 0;
         this._images = [];
     },
@@ -25,40 +24,58 @@ PhotoEditor.Thumbnail.prototype = {
         this._progress = document.getElementById("progress");
         this._progressArea = $(".progress_area");
     },
-    _attachEvent: function () {
-
-    },
     createImage : function(files){
         var fileCount = files.length;
         for (var fi = 0; fi < fileCount; fi += 1) {
             var file = files[fi];
             var reader = new FileReader();
-            reader.onloadend = $.proxy(this._onloadendImage, this, fi);
-            reader.onprogress = $.proxy(this._onprogressImage, this);
+            reader.onloadstart = $.proxy(this._onloadstartImage, this);
+            reader.onload = $.proxy(this._onloadImage, this, fi);
+            reader.onerror = $.proxy(this._onerrorImage, this);
+            if(fi === fileCount - 1){
+                reader.onloadend = $.proxy(this._onloadendLastImage, this);
+            }else{
+                reader.onloadend = $.proxy(this._onloadendImage, this);
+            }
             reader.readAsDataURL(file);
+
+
         }
-    },
-    deleteImages : function(){
-        this.waThumbnail.find("img").remove();
-    },
-    _onprogressImage : function(progressEvent){
-        this._progressArea.show();
-        if(progressEvent.lengthComputable){
-            this._progress.max = progressEvent.total;
-            this._progress.value = progressEvent.loaded;
-        }
-    },
-    _onloadendImage : function(index, progressEvent){
-        var img = new PhotoEditor.Image({"key" : index,
-                                         "fileSrc" : progressEvent.target.result,
-                                         "callback" : $.proxy(this._callbackCreateImage, this)});
-        this._progress.max = 1;
-        this._progress.value = 1;
-        this._progressArea.hide();
     },
     _callbackCreateImage : function(image){
         this._addImage(image);
         this._setFileToThumbnail(image);
+    },
+    _onloadstartImage : function(progressEvent){
+        this._showProgress();
+        if(progressEvent.lengthComputable){
+            this._progress.max += progressEvent.total;
+        }
+    },
+    _onloadImage : function(index, progressEvent){
+        var img = new PhotoEditor.Image({"key" : index,
+                                         "fileSrc" : progressEvent.target.result,
+                                         "callback" : $.proxy(this._callbackCreateImage, this)});
+
+    },
+    _onerrorImage : function(event){
+        console.log(event.target.error);
+    },
+    _onloadendImage:function(progressEvent){
+        console.log(progressEvent.target.readyState);
+        if(progressEvent.target.readyState === FileReader.DONE){
+            this._progress.value += progressEvent.loaded;
+        }
+    },
+    _onloadendLastImage : function(progressEvent){
+        if(progressEvent.lengthComputable){
+            this._progress.value += progressEvent.loaded;
+        }
+
+        setTimeout($.proxy(this._hideProgress, this), 2000);
+    },
+    deleteImages : function(){
+        this.waThumbnail.find("img").remove();
     },
     _addImage : function(image){
         this._images.push(image);
@@ -71,5 +88,11 @@ PhotoEditor.Thumbnail.prototype = {
                 return true;
             }
         }
+    },
+    _showProgress : function(){
+        this._progressArea.show();
+    },
+    _hideProgress : function(){
+        this._progressArea.hide();
     }
 };
